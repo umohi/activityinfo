7878aa31ff4f1f9b2756a69e2d099de404910858
Yuriy Zabrovarnyy <yzabrovarniy@gmail.com>
2014-02-12 12:19:52 +0200

    AI-482 - changing list control design (intermediate state)

--- server/src/main/java/org/activityinfo/ui/full/client/widget/form/FormFieldWidgetReferenceListPanel.java
+++ server/src/main/java/org/activityinfo/ui/full/client/widget/form/FormFieldWidgetReferenceListPanel.java
@@ -24,13 +24,13 @@ package org.activityinfo.ui.full.client.widget.form;
 import com.google.common.collect.Maps;
 import com.google.common.collect.Sets;
 import com.google.gwt.core.client.GWT;
-import com.google.gwt.event.dom.client.ChangeEvent;
-import com.google.gwt.event.dom.client.ChangeHandler;
+import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.logical.shared.ValueChangeEvent;
 import com.google.gwt.event.logical.shared.ValueChangeHandler;
 import com.google.gwt.event.shared.HandlerRegistration;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.uibinder.client.UiHandler;
 import com.google.gwt.user.client.ui.*;
 import org.activityinfo.api2.shared.Cuid;
 import org.activityinfo.api2.shared.form.FormInstance;
@@ -54,18 +54,23 @@ public class FormFieldWidgetReferenceListPanel extends Composite implements Form
 
     private final Map<String, Cuid> labelToCuidMap = Maps.newHashMap();
 
-    private ListBox dropBox;
-    private SuggestBox suggestBox;
-
     @UiField
-    HorizontalPanel panel;
+    ListBox selectedList;
+    @UiField(provided = true)
+    SuggestBox suggestBox;
+    @UiField
+    Button addButton;
+    @UiField
+    Button removeButton;
 
-    private final Map<Cuid, CheckBox> controls = Maps.newHashMap();
+    //    private final Map<Integer, Cuid> listIndexToCuidMap = Maps.newHashMap();
+    private final MultiWordSuggestOracle oracle = new MultiWordSuggestOracle();
     private List<FormInstance> instances;
 
     public FormFieldWidgetReferenceListPanel() {
         TransitionUtil.ensureBootstrapInjected();
         initWidget(uiBinder.createAndBindUi(this));
+        suggestBox = new SuggestBox(oracle);
     }
 
     public FormFieldWidgetReferenceListPanel(List<FormInstance> formInstances) {
@@ -75,35 +80,27 @@ public class FormFieldWidgetReferenceListPanel extends Composite implements Form
 
     public void init(List<FormInstance> instances) {
         this.instances = instances;
-        this.dropBox = new ListBox(true);
-        this.suggestBox = createSuggestBox(instances);
-
-        for (FormInstance instance : instances) {
-            dropBox.addItem(FormInstanceLabeler.getLabel(instance), instance.getId().asString());
-        }
-        dropBox.addChangeHandler(new ChangeHandler() {
-            @Override
-            public void onChange(ChangeEvent event) {
-                fireEvent(new CuidValueChangeEvent(getValue()));
-            }
-        });
+        initOracle(instances);
     }
 
-    private SuggestBox createSuggestBox(List<FormInstance> instances) {
-        final MultiWordSuggestOracle oracle = new MultiWordSuggestOracle();
+    @UiHandler("addButton")
+    public void onAdd(ClickEvent event) {
+        // todo
+        fireEvent(new CuidValueChangeEvent(Sets.newHashSet(getValue())));
+    }
+
+    @UiHandler("removeButton")
+    public void onRemove(ClickEvent event) {
+        // todo
+        fireEvent(new CuidValueChangeEvent(Sets.newHashSet(getValue())));
+    }
+
+    private void initOracle(List<FormInstance> instances) {
         for (FormInstance instance : instances) {
             final String labelValue = FormInstanceLabeler.getLabel(instance);
             oracle.add(labelValue);
             labelToCuidMap.put(labelValue, instance.getId());
         }
-        final SuggestBox box = new SuggestBox(oracle);
-        box.addValueChangeHandler(new ValueChangeHandler<String>() {
-            @Override
-            public void onValueChange(ValueChangeEvent<String> event) {
-                fireEvent(new CuidValueChangeEvent(Sets.newHashSet(getValue())));
-            }
-        });
-        return box;
     }
 
     public List<FormInstance> getInstances() {
@@ -112,24 +109,22 @@ public class FormFieldWidgetReferenceListPanel extends Composite implements Form
 
     @Override
     public void setReadOnly(boolean readOnly) {
-        for (CheckBox checkBox : controls.values()) {
-            checkBox.setEnabled(!readOnly);
-        }
+        final boolean enabled = !readOnly;
+        selectedList.setEnabled(enabled);
+        suggestBox.setEnabled(enabled);
+        addButton.setEnabled(enabled);
+        removeButton.setEnabled(enabled);
     }
 
     @Override
     public boolean isReadOnly() {
-        return !controls.isEmpty() && !controls.values().iterator().next().isEnabled();
+        return !selectedList.isEnabled();
     }
 
     @Override
     public Set<Cuid> getValue() {
         final Set<Cuid> value = Sets.newHashSet();
-        for (Map.Entry<Cuid, CheckBox> entry : controls.entrySet()) {
-            if (entry.getValue().getValue()) {
-                value.add(entry.getKey());
-            }
-        }
+        // todo : resolve index from list
         return value;
     }
 
@@ -142,9 +137,7 @@ public class FormFieldWidgetReferenceListPanel extends Composite implements Form
     public void setValue(Set<Cuid> value, boolean fireEvents) {
         if (value != null && !value.isEmpty()) {
             final Set<Cuid> oldValue = getValue();
-            for (Map.Entry<Cuid, CheckBox> entry : controls.entrySet()) {
-                entry.getValue().setValue(value.contains(entry.getKey()));
-            }
+            // todo : resolve index from list
 
             if (fireEvents) {
                 CuidValueChangeEvent.fireIfNotEqual(this, oldValue, value);
