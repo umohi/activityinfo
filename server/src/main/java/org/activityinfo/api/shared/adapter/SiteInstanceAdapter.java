289e2b15abcc17f56dfaace8746321e76e113865
Yuriy Zabrovarnyy <yzabrovarniy@gmail.com>
2014-02-25 12:00:33 +0200

    AI-499 : temp fix for FormInstance while binding approach is not introduced

--- server/src/main/java/org/activityinfo/api/shared/adapter/SiteInstanceAdapter.java
+++ server/src/main/java/org/activityinfo/api/shared/adapter/SiteInstanceAdapter.java
@@ -1,9 +1,13 @@
 package org.activityinfo.api.shared.adapter;
 
+import com.google.common.collect.Sets;
 import org.activityinfo.api.shared.adapter.bindings.SiteBinding;
 import org.activityinfo.api.shared.model.*;
+import org.activityinfo.api2.shared.Cuid;
 import org.activityinfo.api2.shared.form.FormInstance;
 import org.activityinfo.api2.shared.function.BiFunction;
+
+import java.util.Set;
 
 
 public class SiteInstanceAdapter extends BiFunction<SchemaDTO, SiteDTO, FormInstance> {
@@ -13,8 +17,39 @@ public class SiteInstanceAdapter extends BiFunction<SchemaDTO, SiteDTO, FormInst
 
         ActivityDTO activity = schema.getActivityById(site.getActivityId());
         SiteBinding binding = new SiteBinding(activity);
+        final FormInstance formInstance = binding.newInstance(site);
+        tempFixUntilBindingApproachIsNotIntroduced(formInstance, site, activity);
+        return formInstance;
+    }
 
-        return binding.newInstance(site);
+    private void tempFixUntilBindingApproachIsNotIntroduced(FormInstance instance, SiteDTO site, ActivityDTO activity) {
+        for (String propertyName : site.getPropertyNames()) {
+            if (propertyName.startsWith(IndicatorDTO.PROPERTY_PREFIX)) {
+                int indicatorId = IndicatorDTO.indicatorIdForPropertyName(propertyName);
+                Double value = site.getIndicatorValue(indicatorId);
+
+                if (value != null) {
+                    instance.set(CuidAdapter.indicatorField(indicatorId), value);
+                }
+            } else if (propertyName.startsWith(AttributeDTO.PROPERTY_PREFIX)) {
+                final int attributeId = AttributeDTO.idForPropertyName(propertyName);
+                if (site.getAttributeValue(attributeId)) {
+                    final Cuid attributeCuid = CuidAdapter.attributeId(attributeId);
+
+                    for (AttributeGroupDTO attributeGroup : activity.getAttributeGroups()) {
+                        if (attributeGroup.getAttributeIds().contains(attributeId)) {
+                            final Cuid attributeGroupCuid = CuidAdapter.attributeGroupField(activity, attributeGroup);
+                            final Object set = instance.get(attributeGroupCuid);
+                            if (set instanceof Set) {
+                                ((Set) set).add(attributeCuid);
+                            } else {
+                                instance.set(attributeGroupCuid, Sets.newHashSet(attributeCuid));
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
 
 }
