b03651b0ece40ba640f6e4e4da0432a5052cf481
alex@bedatadriven.com <alex@bedatadriven.com>
2014-03-01 20:30:32 +0100

    [AI-402] CSS and layout fixes for importer

--- server/src/main/java/org/activityinfo/ui/full/client/importer/ui/source/ChooseSourcePage.java
+++ server/src/main/java/org/activityinfo/ui/full/client/importer/ui/source/ChooseSourcePage.java
@@ -8,6 +8,7 @@ import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.uibinder.client.UiHandler;
 import com.google.gwt.user.cellview.client.DataGrid;
 import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.ResizeComposite;
 import com.google.gwt.user.client.ui.TextArea;
 import com.google.gwt.user.client.ui.Widget;
 import org.activityinfo.ui.full.client.importer.data.PastedTable;
@@ -20,7 +21,7 @@ import org.activityinfo.ui.full.client.importer.ui.PageChangedEvent;
  * Start page for the table import process that prompts the user
  * for pasted table data
  */
-public class ChooseSourcePage extends Composite implements ImportPage {
+public class ChooseSourcePage extends ResizeComposite implements ImportPage {
 
 
     private static ChoosePageUiBinder uiBinder = GWT
