0dc9352b65c613beb9ddb9d41fc8f9068909b9d1
Yuriy Zabrovarnyy <yzabrovarniy@gmail.com>
2014-03-03 16:25:47 +0200

    AI-499 : stick to java.util.Date instead of LocalDate

--- server/src/main/java/org/activityinfo/api/shared/adapter/bindings/SimpleFieldBinding.java
+++ server/src/main/java/org/activityinfo/api/shared/adapter/bindings/SimpleFieldBinding.java
@@ -5,12 +5,11 @@ import org.activityinfo.api.shared.model.EntityDTO;
 import org.activityinfo.api2.shared.Cuid;
 import org.activityinfo.api2.shared.form.FormInstance;
 
-import java.util.Date;
 import java.util.Map;
 
 /**
-* Created by alex on 2/22/14.
-*/
+ * Created by alex on 2/22/14.
+ */
 public class SimpleFieldBinding implements FieldBinding<EntityDTO> {
     private final Cuid fieldId;
     private final String propertyName;
@@ -26,7 +25,7 @@ public class SimpleFieldBinding implements FieldBinding<EntityDTO> {
         if (value != null) {
             if (value instanceof LocalDate) {
                 final LocalDate localDate = (LocalDate) value;
-                instance.set(fieldId, new Date(localDate.getYear(), localDate.getMonthOfYear(), localDate.getDayOfMonth()));
+                instance.set(fieldId, localDate.atMidnightInMyTimezone());
             } else {
                 instance.set(fieldId, value);
             }
