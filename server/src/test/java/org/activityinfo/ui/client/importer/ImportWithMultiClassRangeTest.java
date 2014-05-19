46f6defb23df8892a9aa10af1c61e910d99e958e
Yuriy Zabrovarnyy <yzabrovarniy@gmail.com>
2014-05-19 11:39:14 +0300

    AI-406 : preparing test case for multi class range

--- server/src/test/java/org/activityinfo/ui/client/importer/ImportWithMultiClassRangeTest.java
+++ server/src/test/java/org/activityinfo/ui/client/importer/ImportWithMultiClassRangeTest.java
@@ -1,10 +1,13 @@
 package org.activityinfo.ui.client.importer;
 
+import com.google.common.base.Charsets;
+import com.google.common.io.Resources;
 import org.activityinfo.core.shared.Cuid;
 import org.activityinfo.core.shared.form.tree.*;
 import org.activityinfo.fixtures.InjectionSupport;
 import org.activityinfo.legacy.shared.adapter.CuidAdapter;
 import org.activityinfo.server.database.OnDataSet;
+import org.activityinfo.ui.client.component.importDialog.data.PastedTable;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -27,41 +30,39 @@ public class ImportWithMultiClassRangeTest extends AbstractImporterTest {
         FormTree formTree = assertResolves(formTreeBuilder.apply(NFI_DISTRIBUTION_FORM_CLASS));
         FormTreePrettyPrinter.print(formTree);
 
-//        Hierarchy hierarchy = new Hierarchy(formTree.getNodeByPath(new FieldPath(CuidAdapter.locationField(33))));
-//        HierarchyPrettyPrinter.prettyPrint(hierarchy);
-//
-//
+        Hierarchy hierarchy = new Hierarchy(formTree.getNodeByPath(new FieldPath(CuidAdapter.locationField(33))));
+        HierarchyPrettyPrinter.prettyPrint(hierarchy);
+
+
 //        ColumnChoicePresenter choicePresenter = new ColumnChoicePresenter(
 //                importModel = new ImportModel(formTree));
 //
-//        for(FieldModel fieldModel : choicePresenter.getOptions()) {
+//        for (FieldModel fieldModel : choicePresenter.getOptions()) {
 //            System.out.println(fieldModel.getLabel());
 //        }
-//
-//        // Step 1: User pastes in data to import
-//        PastedTable source = new PastedTable(
-//                Resources.toString(getResource("org/activityinfo/core/shared/importing/nfi.csv"),
-//                        Charsets.UTF_8));
-//        importModel.setSource(source);
-//
-//        dumpList("COLUMNS", source.getColumns());
-//
-//        importModel.setColumnBinding(field("End Date"), columnIndex("Date1"));
-//        importModel.setColumnBinding(field("Partner Name"), columnIndex("Partner"));
-//        importModel.setColumnBinding(field("Localité Name"), columnIndex("Localité"));
-//        importModel.setColumnBinding(field("Province Name"), columnIndex("Province"));
-//        importModel.setColumnBinding(field("District Name"), columnIndex("District"));
-//        importModel.setColumnBinding(field("Territoire Name"), columnIndex("Territoire"));
-//        importModel.setColumnBinding(field("Territoire Name"), columnIndex("Territoire"));
-//        importModel.setColumnBinding(field("Secteur Name"), columnIndex("Secteur"));
-//        importModel.setColumnBinding(field("Groupement Name"), columnIndex("Groupement"));
-//        importModel.setColumnBinding(field("Zone de Santé Name"), columnIndex("Zone de Santé"));
-//        importModel.setColumnBinding(field("Nombre de ménages ayant reçu une assistance en NFI"),
-//                columnIndex("Nombre de ménages ayant reçu une assistance en NFI"));
-//
-//
-//        matchReferences();
-//        validateRows();
+
+        // Step 1: User pastes in data to import
+        PastedTable source = new PastedTable(
+                Resources.toString(getResource("org/activityinfo/core/shared/importing/nfi.csv"), Charsets.UTF_8));
+        importModel.setSource(source);
+
+        dumpList("COLUMNS", source.getColumns());
+
+        importModel.setColumnAction(columnIndex("Date1"), target("End Date"));
+        importModel.setColumnAction(columnIndex("Partner"), target("Partner Name"));
+        importModel.setColumnAction(columnIndex("Localité"), target("Localité Name"));
+        importModel.setColumnAction(columnIndex("Province"), target("Province Name"));
+        importModel.setColumnAction(columnIndex("District"), target("District Name"));
+        importModel.setColumnAction(columnIndex("Territoire"), target("Territoire Name"));
+        importModel.setColumnAction(columnIndex("Territoire"), target("Territoire Name"));
+        importModel.setColumnAction(columnIndex("Secteur"), target("Secteur Name"));
+        importModel.setColumnAction(columnIndex("Groupement"), target("Groupement Name"));
+        importModel.setColumnAction(columnIndex("Zone de Santé"), target("Zone de Santé Name"));
+        importModel.setColumnAction(columnIndex("Nombre de ménages ayant reçu une assistance en NFI"), target("Nombre de ménages ayant reçu une assistance en NFI"));
+
+
+        matchReferences();
+        validateRows();
 //        showValidationGrid();
 
 
