fc70554f8117233663e20e43719037808b19dd5f
alex@bedatadriven.com <alex@bedatadriven.com>
2014-05-19 15:11:49 +0200

    Added test case for real multi-class import

--- server/src/test/java/org/activityinfo/ui/client/importer/ImportWithMultiClassRangeTest.java
+++ server/src/test/java/org/activityinfo/ui/client/importer/ImportWithMultiClassRangeTest.java
@@ -5,18 +5,25 @@ import com.extjs.gxt.ui.client.Style;
 import com.extjs.gxt.ui.client.data.SortInfo;
 import com.google.common.base.Charsets;
 import com.google.common.base.Joiner;
+import com.google.common.collect.Sets;
 import com.google.common.io.Resources;
 import org.activityinfo.core.server.type.converter.JvmConverterFactory;
 import org.activityinfo.core.shared.Cuid;
+import org.activityinfo.core.shared.criteria.ClassCriteria;
+import org.activityinfo.core.shared.form.FormInstance;
 import org.activityinfo.core.shared.form.tree.*;
 import org.activityinfo.core.shared.importing.model.ImportModel;
 import org.activityinfo.core.shared.importing.strategy.FieldImportStrategies;
 import org.activityinfo.core.shared.importing.validation.ValidatedRowTable;
 import org.activityinfo.fixtures.InjectionSupport;
+import org.activityinfo.fp.client.Promise;
 import org.activityinfo.legacy.shared.adapter.CuidAdapter;
+import org.activityinfo.legacy.shared.adapter.LocationClassAdapter;
 import org.activityinfo.legacy.shared.command.Filter;
+import org.activityinfo.legacy.shared.command.GetLocations;
 import org.activityinfo.legacy.shared.command.GetSchema;
 import org.activityinfo.legacy.shared.command.GetSites;
+import org.activityinfo.legacy.shared.command.result.LocationResult;
 import org.activityinfo.legacy.shared.command.result.SiteResult;
 import org.activityinfo.legacy.shared.model.ActivityDTO;
 import org.activityinfo.legacy.shared.model.SchemaDTO;
@@ -24,14 +31,19 @@ import org.activityinfo.legacy.shared.model.SiteDTO;
 import org.activityinfo.server.database.OnDataSet;
 import org.activityinfo.ui.client.component.importDialog.Importer;
 import org.activityinfo.ui.client.component.importDialog.data.PastedTable;
+import org.hamcrest.Matchers;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import java.io.IOException;
+import java.util.List;
+import java.util.Set;
 
 import static com.google.common.io.Resources.getResource;
 import static org.activityinfo.core.client.PromiseMatchers.assertResolves;
 import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.hasItem;
+import static org.hamcrest.CoreMatchers.hasItems;
 import static org.junit.Assert.assertThat;
 
 @RunWith(InjectionSupport.class)
@@ -39,6 +51,11 @@ import static org.junit.Assert.assertThat;
 public class ImportWithMultiClassRangeTest extends AbstractImporterTest {
 
     public static final Cuid NFI_DISTRIBUTION_FORM_CLASS = CuidAdapter.activityFormClass(33);
+
+    public static final Cuid SCHOOL_FORM_CLASS = CuidAdapter.locationFormClass(2);
+
+    public static final Cuid ADMIN_FIELD = LocationClassAdapter.getAdminFieldId(SCHOOL_FORM_CLASS);
+
 
     // admin levels
     public static final int PROVINCE_LEVEL = 1;
@@ -56,8 +73,17 @@ public class ImportWithMultiClassRangeTest extends AbstractImporterTest {
     public static final int ECHO = 400;
     public static final int DEPLACEMENT = 63;
 
+    private static final Cuid PROVINCE_KATANGA = CuidAdapter.entity(141804);
+    private static final Cuid DISTRICT_TANGANIKA = CuidAdapter.entity(141845);
+    private static final Cuid TERRITOIRE_KALEMIE = CuidAdapter.entity(141979);
+    private static final Cuid SECTEUR_TUMBWE = CuidAdapter.entity(141979);
+    private static final Cuid GROUPEMENT_LAMBO_KATENGA = CuidAdapter.entity(148235);
+    private static final Cuid ZONE_SANTE_NYEMBA = CuidAdapter.entity(212931);
+
+    private List<FormInstance> instances;
+
     @Test
-    public void test() throws IOException {
+    public void testSimple() throws IOException {
 
         setUser(3);
 
@@ -118,5 +144,70 @@ public class ImportWithMultiClassRangeTest extends AbstractImporterTest {
 
         assertThat(lastSite.getAttributeValue(ECHO), equalTo(false));
         assertThat(lastSite.getAttributeValue(DEPLACEMENT), equalTo(true));
+    }
+
+    @Test
+    public void testMulti() throws IOException {
+
+        setUser(3);
+
+        FormTree formTree = assertResolves(formTreeBuilder.apply(SCHOOL_FORM_CLASS));
+        FormTreePrettyPrinter.print(formTree);
+
+        importModel = new ImportModel(formTree);
+        importer = new Importer(resourceLocator, formTree, FieldImportStrategies.get(JvmConverterFactory.get()));
+
+
+        // Step 1: User pastes in data to import
+        PastedTable source = new PastedTable(
+                Resources.toString(getResource("org/activityinfo/core/shared/importing/school-import.csv"), Charsets.UTF_8));
+        importModel.setSource(source);
+
+        dumpList("COLUMNS", source.getColumns());
+
+        importModel.setColumnAction(columnIndex("School"), target("Name"));
+
+        // Province is at the root of both hierarchies
+        importModel.setColumnAction(columnIndex("Province"), target("Province Name"));
+
+        // Admin hierarchy
+        importModel.setColumnAction(columnIndex("District"), target("District Name"));
+        importModel.setColumnAction(columnIndex("Territoire"), target("Territoire Name"));
+        importModel.setColumnAction(columnIndex("Secteur"), target("Secteur Name"));
+        importModel.setColumnAction(columnIndex("Groupement"), target("Secteur Name"));
+
+        // health ministry hierarchy
+        importModel.setColumnAction(columnIndex("Zone de Santé"), target("Zone de Santé Name"));
+
+        ValidatedRowTable validatedResult = assertResolves(importer.validateRows(importModel));
+        showValidationGrid(validatedResult);
+
+        assertResolves(importer.persist(importModel));
+
+        instances = assertResolves(resourceLocator.queryInstances(new ClassCriteria(SCHOOL_FORM_CLASS)));
+        assertThat(instances.size(), equalTo(3));
+
+        assertThat(school("P"), equalTo(set(PROVINCE_KATANGA)));
+        assertThat(school("D"), equalTo(set(DISTRICT_TANGANIKA)));
+        assertThat(school("T"), equalTo(set(TERRITOIRE_KALEMIE)));
+        assertThat(school("S"), equalTo(set(SECTEUR_TUMBWE)));
+        assertThat(school("G"), equalTo(set(GROUPEMENT_LAMBO_KATENGA)));
+        assertThat(school("GZ"), equalTo(set(GROUPEMENT_LAMBO_KATENGA, ZONE_SANTE_NYEMBA)));
+        assertThat(school("TZ"), equalTo(set(TERRITOIRE_KALEMIE, ZONE_SANTE_NYEMBA)));
+
+
+    }
+
+    private Set<Cuid> school(String name) {
+        for(FormInstance instance : instances) {
+            if(name.equals(instance.getString(LocationClassAdapter.getNameFieldId(SCHOOL_FORM_CLASS)))) {
+                return instance.getReferences(ADMIN_FIELD);
+            }
+        }
+        throw new AssertionError("No instance with name " + name);
+    }
+
+    public static Set<Cuid> set(Cuid... cuids) {
+        return Sets.newHashSet(cuids);
     }
 }
